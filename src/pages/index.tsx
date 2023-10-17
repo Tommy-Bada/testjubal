e5d0f8d02a88bd902c9f47b3a0f48480087568f4
Tomiwa Bada <tomiwabd@gmail.com>
2023-10-17 22:51:27 +0100

    Pages link

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,25 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <>Hello</>;
+  return (
+    <div className="flex justify-center items-center">
+      <div>
+        <Link href="/signin">
+          <h1 className="text-[2rem] my-4">Sign Up Page</h1>
+        </Link>
+        <Link href="/login">
+          <h1 className="text-[2rem] my-4">Login Page</h1>
+        </Link>
+        <Link href="/forgotpassword">
+          <h1 className="text-[2rem] my-4">Forgot Password Page</h1>
+        </Link>
+        <Link href="/passwordverification">
+          <h1 className="text-[2rem] my-4">Password Verification Page</h1>
+        </Link>
+        <Link href="/createnewpassword">
+          <h1 className="text-[2rem] my-4">Create New Password Page</h1>
+        </Link>
+      </div>
+    </div>
+  );
 }
