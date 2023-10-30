e1bf5bbb025a292f2db6e53b0c74b600d6eb997f
Tomiwa Bada <tomiwabd@gmail.com>
2023-10-30 16:26:10 +0100

    dashboard

--- src/pages/manager.tsx
+++ src/pages/manager.tsx
@@ -1,4 +1,5 @@
 import SideBar from "@/features/Dashboard/components/SideBar";
+<<<<<<< HEAD
 import TalentHeader from "@/features/Dashboard/components/TalentHeader";
 import ManagerTopData from "@/features/Dashboard/components/ManagerTopData";
 import ToDoList from "@/features/Dashboard/components/ToDoList";
@@ -15,6 +16,18 @@ export default function Dashboard() {
           <ManagerGigBox />
           <ToDoList />
         </div>
+=======
+import DashboardHeader from "@/features/Dashboard/components/DashboardHeader";
+
+export default function Manager() {
+  return (
+    <div className="flex bg-gradient-to-r from-[#3F288339] to-[#2DA5A439] py-[3rem] px-[5rem]">
+      <SideBar />
+      <div className="w-[76%] bg-white p-[2.5rem] ml-[24%] rounded-[2rem] ">
+        <DashboardHeader />
+        {/* <MangagerTopData /> */}
+        <div className="flex justify-between"></div>
+>>>>>>> 67eec19139fd2da9645c708843b0128bfbe1c1cf
       </div>
     </div>
   );
