5e5b3fac1136b9dff4568f15481f34296b29cb4a
Tomiwa Bada <tomiwabd@gmail.com>
2023-11-10 09:31:20 +0100

    Test dev

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,16 +1,17 @@
-import { config } from '@/config';
-import * as React from 'react';
+import { config } from "@/config";
+import * as React from "react";
 import Image from "next/image";
 
-require('dotenv').config();
+require("dotenv").config();
+console.log(config.apiBaseUrl);
 
-const NotFoundPage = () => {  
+const NotFoundPage = () => {
   return (
     <main>
-      <section className='bg-white'>
-        <div className='flex flex-col items-center justify-center min-h-screen text-center text-black layout'>
-          <div className='text-red-500 drop-shadow-glow animate-flicker' />
-          <h1 className='mt-8 text-4xl md:text-6xl'>Page Not Found</h1>
+      <section className="bg-white">
+        <div className="flex flex-col items-center justify-center min-h-screen text-center text-black layout">
+          <div className="text-red-500 drop-shadow-glow animate-flicker" />
+          <h1 className="mt-8 text-4xl md:text-6xl">Page Not Found</h1>
         </div>
       </section>
     </main>
